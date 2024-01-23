ad683ee49d7a6de986e4ba4d1a83df56f15794c8
solomonster <ismometrical@gmail.com>
2024-01-23 13:26:38 +0100

    Added the repository interface for our Bookmarks

---
+++ src/main/java/com/ismometrical/bookmarks/domain/BookmarkRepository.java
@@ -0,0 +1,7 @@
+package com.ismometrical.bookmarks.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
+
+}
