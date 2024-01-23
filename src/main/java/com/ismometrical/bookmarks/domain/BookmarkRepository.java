909dabb99279df22239a508b55d5f76980465f1e
solomonster <ismometrical@gmail.com>
2024-01-24 00:13:33 +0100

    defined method to fetch bookmark with pagination

--- src/main/java/com/ismometrical/bookmarks/domain/BookmarkRepository.java
+++ src/main/java/com/ismometrical/bookmarks/domain/BookmarkRepository.java
@@ -1,7 +1,17 @@
 package com.ismometrical.bookmarks.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 interface BookmarkRepository extends JpaRepository<Bookmark,Long> {
+
+    @Query("""
+               SELECT
+                new com.ismometrical.bookmarks.domain.BookmarkDTO(b.id, b.title, b.url, b.createdAt)
+               FROM Bookmark b
+            """)
+    Page<BookmarkDTO> findBookmarks(Pageable pageable);
 
 }
