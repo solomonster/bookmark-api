4cf10c4e2aeac584859eb5a87c32f1ec0566881b
solomonster <ismometrical@gmail.com>
2024-01-24 15:41:31 +0100

    Implemented findBookmarks method

--- src/main/java/com/ismometrical/bookmarks/domain/BookmarkService.java
+++ src/main/java/com/ismometrical/bookmarks/domain/BookmarkService.java
@@ -1,6 +1,10 @@
 package com.ismometrical.bookmarks.domain;
 
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +16,22 @@ public class BookmarkService {
     {
         this.bookmarkRepository = bookmarkRepository;
     }
-    
+    public PagedResult<BookmarkDTO> findBookmarks(FindBookmarksQuery query) {
+        Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
+        //from user POV, page number starts from 1, but for Spring Data JPA page number starts from 0.
+        int pageNo = query.pageNo() > 0 ? query.pageNo() - 1 : 0;
+        Pageable pageable = PageRequest.of(pageNo, query.pageSize(), sort);
+        Page<BookmarkDTO> page = bookmarkRepository.findBookmarks(pageable);
+        return new PagedResult<>(
+                page.getContent(),
+                page.getTotalElements(),
+                page.getNumber() + 1, // for user page number starts from 1
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast(),
+                page.hasNext(),
+                page.hasPrevious()
+        );
+    }
 
 }
